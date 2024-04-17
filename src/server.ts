4a1835cfa022ef3ebc4b2d3cbd41d96616f5f988
Manuel dos Santos <manuelds@outlook.pt>
2024-04-17 01:19:51 +0100

    feat: :sparkles: add auth-middleware

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastify, FastifyInstance } from "fastify";
+import { contactRoutes } from "./routes/contact.routes";
 import { userRoutes } from "./routes/user.routes";
 
 const server: FastifyInstance = fastify({ logger: true })
@@ -8,7 +9,9 @@ server.register(userRoutes, {
     prefix: 'users'
 
 })
-
+server.register(contactRoutes, {
+    prefix: "contacts"
+})
 server.listen({ port: _port }, () => {
     console.log(`Server is up on ${_port}`)
 })
